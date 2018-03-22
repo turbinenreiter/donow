4f32f2dcdc660ac688d6a5944743d82e373c7b1e
Sebastian Plamauer <oeplse@gmail.com>
2018-03-22 16:14:49 +0100

    Package for PyPI

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name='donow',
+    version='0.1',
+    description='make-like calling for functions in shell-scripts',
+    url='https://github.com/turbinenreiter/donow',
+    author='Sebastian Plamauer',
+    author_email='oeplse@gmail.com',
+    license='MIT',
+    scripts=['donow']
+)
